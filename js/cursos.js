1c3487df9facade16869001b2abf5c5deaad7b4f
hugo issao uraga <hugouraga61@gmail.com>
2018-08-23 15:26:54 -0300

    ajustes

--- js/cursos.js
+++ js/cursos.js
@@ -2,6 +2,6 @@ var modulo = angular.module('modulo',[]);
 
 modulo.controller("MyController", function($scope, $http){
 	$http.get("js/cursos.json").then(function(response){
-		$scope.cursos = response.data.cursos;
+		$scope.cursos = response.data.cursos
 	})
 })
