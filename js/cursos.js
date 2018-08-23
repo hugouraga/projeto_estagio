1eaff043aa1a878bd698e8889753d7d855a56a52
hugo issao uraga <hugouraga61@gmail.com>
2018-08-23 15:10:19 -0300

    projeto

---
+++ js/cursos.js
@@ -0,0 +1,7 @@
+var modulo = angular.module('modulo',[]);
+
+modulo.controller("MyController", function($scope, $http){
+	$http.get("js/cursos.json").then(function(response){
+		$scope.cursos = response.data.cursos;
+	})
+})
